8900bc1dbb23e4daf18b2225277ee76976c9ea49
John Wass <wassj@ctc.com>
2016-09-26 01:26:05 -0400

    writing channel

--- core/src/main/scala/mm2s/ExportBot.scala
+++ core/src/main/scala/mm2s/ExportBot.scala
@@ -1,18 +1,20 @@
 package mm2s
 
+import java.nio.file.Paths
+
 import akka.actor.{Actor, ActorLogging, ActorRef}
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
-import akka.util.Timeout
+import akka.stream.scaladsl.{FileIO, Source}
+import akka.util.{ByteString, Timeout}
 import mm2s.ExportBot.ExportChannel
-import mm2s.conversions.{ChannelConversion, UserConversion}
+import mm2s.conversions.ChannelConversion
 import mm4s.api.ChannelModels.ChannelListing
 import mm4s.api.Streams._
 import mm4s.api._
 import mm4s.bots.api._
 import net.codingwell.scalaguice.ScalaModule
+import spray.json._
 
 import scala.concurrent.duration.DurationInt
 
@@ -25,7 +27,7 @@ object ExportBot {
 
 class ExportBot extends Actor with Bot with ActorLogging {
   implicit val mat: ActorMaterializer = ActorMaterializer()
-
+  implicit val timeout = Timeout(10 seconds)
 
   def uninitialized: Receive = {
     case Ready(api, id) => context.become(ready(api, id))
@@ -39,14 +41,18 @@ class ExportBot extends Actor with Bot with ActorLogging {
       case ExportChannel(chid) ⇒
         import context.system
         import mm4s.api.ChannelProtocols._
-        implicit val timeout = Timeout(10 seconds)
-
+        import protocols._
 
         (api ? ConnectionRequest()).mapTo[Connection].flatMap { c ⇒
-          Channels.__list.via(c).via(response[ChannelListing]).map { l ⇒
-            val users = l.members.values.flatMap(UserConversion(_))
-            val channels = l.channels.flatMap(ChannelConversion(_)).map(_.copy(members = users.map(_.id).toList))
-          }.runWith(Sink.foreach(println))
+          Channels.__list.via(c).via(response[ChannelListing])
+          .map(l ⇒ l.channels.find(_.id == chid) → l.members.keys) // <-- todo; need some additional filtering on the members; will require a change to the model up in mm4s
+          .map {
+            case (Some(ch), m) ⇒
+              ChannelConversion(ch).map(_.copy(members = m.toList)).map(_.toJson)
+            case _ ⇒ None
+          }
+          .flatMapMerge(3, ch ⇒ Source.fromIterator(() ⇒ ch.iterator).map(_.prettyPrint).map(ByteString(_)))
+          .runWith(FileIO.toPath(Paths.get(s"/tmp/slack-channel-$chid.json")))
         }
     }
   }
